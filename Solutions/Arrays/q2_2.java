536d9db5cb59230392b6afb58c21088c927ea651
Rohit-Dubey <113301503+rohitinu6@users.noreply.github.com>
2025-03-14 12:19:49 +0530

    Array Sorting to fetch the Kth element

---
+++ Solutions/Arrays/q2_2.java
@@ -0,0 +1,19 @@
+// Find minimum and maximum in an Array
+public class q2_2 {
+    public static void main(String[] args) {
+        int arr[] = {1,3,6,4,8,6,0,1,95,89,46};
+        int min = arr[0];
+        int max = arr[0];
+        int l = arr.length;
+        for(int i = 1;i<l;i++){
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("the minimum is " + min);
+        System.out.println("the maximum is " + max);
+    }
+}
