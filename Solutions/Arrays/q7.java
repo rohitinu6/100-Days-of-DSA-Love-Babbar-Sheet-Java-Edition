31dc71b013b3dbd7a5a6722920561f150ce28d00
Rohit-Dubey <113301503+rohitinu6@users.noreply.github.com>
2025-03-16 12:55:59 +0530

    cyclically rotate an array by one.

---
+++ Solutions/Arrays/q7.java
@@ -0,0 +1,17 @@
+// cyclically rotate an array by one.
+public class q7 {
+    public static void main(String[] args) {
+        int[] a = { 3, 9, 5, 8, 4, 9, 2 };
+        int l = a.length, temp;
+        int e = 0;
+        int[] res = new int[l];
+        res[l - 1] = a[0];
+        for (int i = 1; i < l; i++) {
+            res[e] = a[i];
+            e++;
+        }
+        for (int i = 0; i < l; i++) {
+            System.out.print(res[i] + " ");
+        }
+    }
+}
